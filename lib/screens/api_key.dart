6819cdd94f0bbaaa675b2ac6b6af3c00456face2
Raul Jimenez Ortega <rjimenez@esri.com>
2023-01-03 13:54:40 +0100

    Minor improvement

--- lib/screens/api_key.dart
+++
@@ -1,3 +0,0 @@
-// Create a free account: https://developers.arcgis.com/sign-up
-// Grab the API from: https://developers.arcgis.com/api-keys/
-final apiKey = 'AAPKbd3b5c597fbb4e6791efa06d12511f794beh6YUYqWLSL-STLyjY085N0dRLUy8l9PNTPnWXj1s_31FvJ8xKnL3l4VwHy0vm';
